1b5c0cd1e036839302cbe95160ff2aafb648b2de
Abu I <84893883+AbudlAbu@users.noreply.github.com>
2021-06-06 16:16:41 +0530

    first commit

---
+++ src/main/java/com/example/HelloHaiApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHaiApplication.class, args);
+		System.out.println("hi Abu");
+	}
+
+}
