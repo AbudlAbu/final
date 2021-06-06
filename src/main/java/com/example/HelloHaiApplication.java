d84025f3833e368143a3b46fbbf2e64a31c962c6
Abu I <84893883+AbudlAbu@users.noreply.github.com>
2021-06-06 16:37:55 +0530

    second

--- src/main/java/com/example/HelloHaiApplication.java
+++ src/main/java/com/example/HelloHaiApplication.java
@@ -9,6 +9,7 @@ public class HelloHaiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloHaiApplication.class, args);
 		System.out.println("hi Abu");
+		System.out.println("hello Abu");
 	}
 
 }
